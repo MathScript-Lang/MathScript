aeb4ee3bd9904c39d617336e3f1df6fb2204cfe4
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2025-04-01 21:06:07 +0200

    Add project files.

---
+++ DebugLevel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathScript
+{
+    internal enum DebugLevel
+    {
+        None,
+        Parser,
+        Lexer,
+        Interpreter
+    }
+
+    internal enum DebugLevelArgument
+    {
+        parser = DebugLevel.Parser,
+        lexer = DebugLevel.Lexer,
+        parser_lexer = DebugLevel.Parser | DebugLevel.Lexer,
+        all = DebugLevel.Parser | DebugLevel.Lexer | DebugLevel.Interpreter
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.Reflection;
+
+namespace MathScript
+{
+    internal class Program
+    {
+        internal static DebugLevel DebugLevel { get; set; } = DebugLevel.None;
+        internal static string ProgramPath { get; set; } = string.Empty;
+        internal static string? OutputPath { get; set; }
+
+        private static async Task Main(string[] args)
+        {
+            Argument<string> pathArgument = new("program", "The MathScript program to compile");
+            Option<string> outputOption = new(
+                ["--output", "-o"],
+                "Compile the MathScript program into the `output` executable"
+            );
+            Option<DebugLevelArgument> debugOption = new(
+                ["--debug", "-d"],
+                "Set the debug level (parser, lexer, parser_lexer, all)"
+            ) { ArgumentHelpName = "debug_level" };
+            Option<bool> versionOption = new(
+                ["--version", "-v"],
+                "Print the version number"
+            );
+
+            RootCommand rootCommand = new("MathScript Interpreter & Compiler")
+            {
+                pathArgument,
+                outputOption,
+                debugOption,
+                versionOption
+            };
+
+            rootCommand.SetHandler((path) => ProgramPath = path, pathArgument);
+            rootCommand.SetHandler((outputPath) => OutputPath = outputPath, outputOption);
+            rootCommand.SetHandler((debugLevel) => DebugLevel = (DebugLevel)debugLevel, debugOption);
+            rootCommand.SetHandler((showVersion) =>
+            {
+                if (showVersion)
+                {
+                    AssemblyName assemblyInfo = Assembly.GetExecutingAssembly().GetName();
+                    Console.WriteLine($"{assemblyInfo.Name} v{assemblyInfo.Version}");
+                }
+            }, versionOption);
+
+            _ = await rootCommand.InvokeAsync(args);
+        }
+    }
+}
